241f136bb20ade7bafba73b6fa98e095589b0dad
blaisepascal287 <130232913+blaisepascal287@users.noreply.github.com>
2023-10-18 19:37:05 +0530

    build location to backend

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,10 @@ const port = 80;
 const path = require('path');
 const __dirname1 = path.resolve();
 
-app.use(express.static(path.join(__dirname1, "../build")));
+app.use(express.static(path.join(__dirname1, "/build")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname1, "../build", "index.html"));
+    res.sendFile(path.join(__dirname1, "build", "index.html"));
 });
 
 app.listen(port, console.log(`App is listening on port`, port));
